84c59f96903dbc08906744da5338a182b93d5259
Kaio Dias <kaiofba65@gmail.com>
2018-12-11 12:14:07 -0200

    Alpha

--- Assets/CustomTerrain.cs
+++ Assets/CustomTerrain.cs
@@ -10,15 +10,111 @@ public class CustomTerrain : MonoBehaviour {
 	public Vector2 randomHeightRange = new Vector2(0,0.1f);
 	public Texture2D heightMapImage;
 	public Vector3 heightMapScale = new Vector3(1,1,1);
+	public bool resetTerrainCheckBox = true;
+
+	//PerlinNoise----------------------------/
+	public float perlinXScale = 0.01f;
+	public float perlinYScale = 0.01f;
+	public int perlinOctaves = 3;
+	public float perlinPersistence = 8;
+	public float perlinHeightScale = 0.09f;
+	public int perlinOffsetX = 0;
+	public int perlinOffsetY = 0;
+
+	//MultiplePerlinNoises------------------/
+	[System.Serializable]
+	public class PerlinParameters{
+		public float mPerlinXscale = 0.01f;
+		public float mPerlinYscale = 0.01f;
+		public int mPerlinOctaves = 3;
+		public float mPerlinPersistence = 8;
+		public float mPerlinHeightScale = 0.09f;
+		public int mPerlinOffsetX = 0;
+		public int mPerlinOffsetY = 0;
+		public bool remove = false;
+	}
+	public List<PerlinParameters> perlinParameters = new List<PerlinParameters>(){
+		new PerlinParameters() //istantiate one so the table dont complain//
+	};
+
+	//Terrain references--------------------/
 	public Terrain terrain;
 	public TerrainData terrainData;
+
+	float[,] getHeightMap(){
+
+		float[,] HeightMap = new float[0,0];
+
+		if (!resetTerrainCheckBox)
+		{
+			HeightMap = terrainData.GetHeights(0,0,terrainData.heightmapWidth,
+		                                                terrainData.heightmapHeight);
+		}
+		else
+		{
+			HeightMap = new float[terrainData.heightmapWidth,terrainData.heightmapHeight];
+		}
+			return HeightMap;
+	}
+
+	public void perlin(){
+		float[,] heightMap = getHeightMap();
+		for (int y = 0; y < terrainData.heightmapHeight; y++)
+		{
+			for (int x = 0; x < terrainData.heightmapWidth; x++)
+			{
+				heightMap[x,y] += Utils.FractionalBM((x + perlinOffsetX) * perlinXScale,
+				                                    (y + perlinOffsetY) * perlinYScale,
+				                                    perlinOctaves,
+													perlinPersistence)* perlinHeightScale;
+			}
+		}
+		terrainData.SetHeights(0,0,heightMap);												
+	}
+
+	public void MultiplePerlin(){
+		float[,] heightMap = getHeightMap();
+		for (int y = 0; y < terrainData.heightmapHeight; y++)
+		{
+			for (int x = 0; x < terrainData.heightmapWidth; x++)
+			{
+				foreach (PerlinParameters p in perlinParameters)
+				{
+					heightMap[x,y] += Utils.FractionalBM((x + p.mPerlinOffsetX) * p.mPerlinXscale,
+					                                     (y + p.mPerlinOffsetY) * p.mPerlinYscale,
+														 p.mPerlinOctaves,
+														 p.mPerlinPersistence) * p.mPerlinHeightScale;
+				}
+			}
+		}
+		terrainData.SetHeights(0,0,heightMap);
+	}
+	
+	public void addNewPerlin(){
+		perlinParameters.Add(new PerlinParameters());
+	}
+
+	public void removeNewPerlin(){
+		List<PerlinParameters> keptPerlinParameters = new List<PerlinParameters>();
+		for (int i = 0; i < perlinParameters.Count; i++)
+		{
+			if (!perlinParameters[i].remove)
+			{
+				keptPerlinParameters.Add(perlinParameters[i]);
+			}
+		}
+		if (keptPerlinParameters.Count == 0) //dont want to keep a single one
+		{
+			keptPerlinParameters.Add(new PerlinParameters()); // add atleast one "because Table"
+		}
+		perlinParameters = keptPerlinParameters;
+	}
 
 
     //att our terrain
 	public void RandomTerrain(){
 
-		float[,] heightMap = terrainData.GetHeights(0,0,terrainData.heightmapWidth,
-		                                                terrainData.heightmapHeight);
+		float[,] heightMap = getHeightMap();
 		for (int x = 0; x < terrainData.heightmapWidth; x++)
 		{
 			for (int y = 0; y < terrainData.heightmapHeight; y++)
@@ -30,13 +126,13 @@ public class CustomTerrain : MonoBehaviour {
 	}
 	
 	public void LoadTexture(){
-		float[,] heightMap = new float[terrainData.heightmapWidth,terrainData.heightmapHeight];
+		float[,] heightMap = getHeightMap();
 
 		for (int x = 0; x < terrainData.heightmapWidth; x++)
 		{
 			for (int y = 0; y < terrainData.heightmapHeight; y++)
 			{
-				heightMap[x,y] = heightMapImage.GetPixel((int)(x * heightMapScale.x),
+				heightMap[x,y] += heightMapImage.GetPixel((int)(x * heightMapScale.x),
 				                                (int)(y*heightMapScale.z)).grayscale * heightMapScale.y;
 			}
 		}
--- Assets/Editor/CustomTerrainEditor.cs
+++ Assets/Editor/CustomTerrainEditor.cs
@@ -5,68 +5,140 @@ using EditorGUITable;
 [CustomEditor(typeof(CustomTerrain))]
 [CanEditMultipleObjects]
 
-public class CustomTerrainEditor : Editor {
+public class CustomTerrainEditor : Editor
+{
 
-	//Properties-------/
-	SerializedProperty randomHeightRange;//reference for the script and the GUI
-	SerializedProperty heightMapScale;
-	SerializedProperty heightMapImage;
+    //Properties-------/
+    SerializedProperty randomHeightRange;//reference for the script and the GUI
+    SerializedProperty heightMapScale;
+    SerializedProperty heightMapImage;
+	 SerializedProperty resetTerrainCheckBox;
 
-	//Foldouts--------/
-	bool showRandom = false; 
-	bool showLoadHeights = false;
-
-	private void OnEnable() {
-		randomHeightRange = serializedObject.FindProperty("randomHeightRange");
-		heightMapScale = serializedObject.FindProperty("heightMapScale");
-		heightMapImage = serializedObject.FindProperty("heightMapImage");
-	}
+    SerializedProperty perlinXScale, perlinYScale,
+                       perlinOffsetX, perlinOffsetY,
+                       perlinOctaves, perlinPersistence,
+                       perlinHeightScale;
     
-	//The GUI Loop, it gets update
-	public override void OnInspectorGUI(){
-		serializedObject.Update();//F
+	GUITableState perlinParametersTable;
+	SerializedProperty perlinParameters;
 
-		CustomTerrain terrain = (CustomTerrain) target;//Reference for our SCRIPT
+    //Foldouts--------/
+    bool showRandom = false;
+    bool showLoadHeights = false;
+    bool showPerlinNoise = false;
+	bool showMultiplePerlins = false;
+
+    //Link Our Variables
+    private void OnEnable()
+    {
+        randomHeightRange = serializedObject.FindProperty("randomHeightRange");
+        heightMapScale = serializedObject.FindProperty("heightMapScale");
+        heightMapImage = serializedObject.FindProperty("heightMapImage");
+        perlinXScale = serializedObject.FindProperty("perlinXScale");
+        perlinYScale = serializedObject.FindProperty("perlinYScale");
+        perlinOffsetX = serializedObject.FindProperty("perlinOffsetX");
+        perlinOffsetY = serializedObject.FindProperty("perlinOffsetY");
+        perlinOctaves = serializedObject.FindProperty("perlinOctaves");
+        perlinPersistence = serializedObject.FindProperty("perlinPersistence");
+        perlinHeightScale = serializedObject.FindProperty("perlinHeightScale");
+		resetTerrainCheckBox = serializedObject.FindProperty("resetTerrainCheckBox");
+		perlinParametersTable = new GUITableState("perlinParameterTable");
+		perlinParameters = serializedObject.FindProperty("perlinParameters");
+    }
+
+    //The GUI Loop, it gets update
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();//F
+
+        CustomTerrain terrain = (CustomTerrain)target;//Reference for our SCRIPT
+
+		//CheckBox For resetTerrain
+		EditorGUILayout.PropertyField(resetTerrainCheckBox);
 
         //Foldout on GUI
-		showRandom = EditorGUILayout.Foldout(showRandom,"Random Heights");
-		if (showRandom)//A foldout need to be followed by a if statement
+        showRandom = EditorGUILayout.Foldout(showRandom, "Random Heights");
+        if (showRandom)//A foldout need to be followed by a if statement
+        {
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+            GUILayout.Label("Set Heights Between Random Values", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(randomHeightRange); //!! link GUI values to our variable
+            if (GUILayout.Button("Random Heights"))
+            {
+                terrain.RandomTerrain();//Method from our customTerrain Script to update our terrain.
+            }
+        }
+
+        //Foldout PerlinNoise
+        showPerlinNoise = EditorGUILayout.Foldout(showPerlinNoise, "Generate Perlin Noise");
+        if (showPerlinNoise)//A foldout need to be followed by a if statement
+        {
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+            GUILayout.Label("Randomize a new Perlin Noise", EditorStyles.boldLabel);
+            EditorGUILayout.Slider(perlinXScale, 0, 1, new GUIContent("X Scale"));//value,min,max
+            EditorGUILayout.Slider(perlinYScale, 0, 1, new GUIContent("Y Scale"));
+            EditorGUILayout.IntSlider(perlinOffsetX, 0, 10000, new GUIContent("X OffSet"));
+            EditorGUILayout.IntSlider(perlinOffsetY, 0, 10000, new GUIContent("Y OffSet"));
+            EditorGUILayout.IntSlider(perlinOctaves, 0, 10, new GUIContent("Octaves"));
+            EditorGUILayout.Slider(perlinPersistence, 0.1f, 10, new GUIContent("Persistence"));
+            EditorGUILayout.Slider(perlinHeightScale, 0, 1, new GUIContent("Height Scale"));
+            if (GUILayout.Button("Generate Perlin"))
+            {
+                terrain.perlin();
+            }
+        }
+
+		//Foldout MultiplePerlinNoise
+		showMultiplePerlins = EditorGUILayout.Foldout(showMultiplePerlins,"Generate Multiple Perlin");
+		if (showMultiplePerlins)
 		{
 			EditorGUILayout.LabelField("",GUI.skin.horizontalSlider);
-			GUILayout.Label("Set Heights Between Random Values", EditorStyles.boldLabel);
-			EditorGUILayout.PropertyField(randomHeightRange); //!! link GUI values to our variable
-			if (GUILayout.Button("Random Heights"))
+			GUILayout.Label("Multiple Perlin Noise", EditorStyles.boldLabel);
+			perlinParametersTable = GUITableLayout.DrawTable(perlinParametersTable,
+			                                            serializedObject.FindProperty("perlinParameters"));
+		    GUILayout.Space(20);
+		    EditorGUILayout.BeginHorizontal();
+			if (GUILayout.Button("+"))
 			{
-				terrain.RandomTerrain();//Method from our customTerrain Script to update our terrain.
+				terrain.addNewPerlin();
+            }
+            if (GUILayout.Button("-"))
+            {
+                terrain.removeNewPerlin();
+            }
+			EditorGUILayout.EndHorizontal();
+			if (GUILayout.Button("Apply Multiple Perlin"))
+			{
+				terrain.MultiplePerlin();
 			}
 		}
-        
-		//Foldout to HeightMap
-		showLoadHeights = EditorGUILayout.Foldout(showLoadHeights,"Load Heights");
-		if (showLoadHeights)//A foldout need to be followed by a if statement
-		{
-			EditorGUILayout.LabelField("",GUI.skin.horizontalSlider);
-			GUILayout.Label("Load Heights From Texture", EditorStyles.boldLabel);
-			EditorGUILayout.PropertyField(heightMapImage);
-			EditorGUILayout.PropertyField(heightMapScale);
-			if (GUILayout.Button("Load Texture"))
-			{
-				terrain.LoadTexture();
-			}
-		}
 
-
-        
-		//resetButton
-		EditorGUILayout.LabelField("",GUI.skin.horizontalSlider);
-		if (GUILayout.Button("Reset Terrain"))
-		{
-			terrain.resetTerrain();
-		}
-		
+        //Foldout to HeightMap
+        showLoadHeights = EditorGUILayout.Foldout(showLoadHeights, "Load Heights");
+        if (showLoadHeights)//A foldout need to be followed by a if statement
+        {
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+            GUILayout.Label("Load Heights From Texture", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(heightMapImage);
+            EditorGUILayout.PropertyField(heightMapScale);
+            if (GUILayout.Button("Load Texture"))
+            {
+                terrain.LoadTexture();
+            }
+        }
 
 
 
-		serializedObject.ApplyModifiedProperties();//L
-	}
+        //resetButton
+        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+        if (GUILayout.Button("Reset Terrain"))
+        {
+            terrain.resetTerrain();
+        }
+
+
+
+
+        serializedObject.ApplyModifiedProperties();//L
+    }
 }
