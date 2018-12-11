9f94bd9e7bd81e43434c37dab49e1cdb646fbd1a
Kaio Dias <kaiofba65@gmail.com>
2018-12-10 22:11:32 -0200

    Initial commit

---
+++ Assets/CustomTerrain.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using System.Linq;
+
+[ExecuteInEditMode]
+public class CustomTerrain : MonoBehaviour {
+
+	public Vector2 randomHeightRange = new Vector2(0,0.1f);
+	public Texture2D heightMapImage;
+	public Vector3 heightMapScale = new Vector3(1,1,1);
+	public Terrain terrain;
+	public TerrainData terrainData;
+
+
+    //att our terrain
+	public void RandomTerrain(){
+
+		float[,] heightMap = terrainData.GetHeights(0,0,terrainData.heightmapWidth,
+		                                                terrainData.heightmapHeight);
+		for (int x = 0; x < terrainData.heightmapWidth; x++)
+		{
+			for (int y = 0; y < terrainData.heightmapHeight; y++)
+			{
+				heightMap[x,y] += UnityEngine.Random.Range(randomHeightRange.x,randomHeightRange.y);
+			}
+		}
+		terrainData.SetHeights(0,0,heightMap);
+	}
+	
+	public void LoadTexture(){
+		float[,] heightMap = new float[terrainData.heightmapWidth,terrainData.heightmapHeight];
+
+		for (int x = 0; x < terrainData.heightmapWidth; x++)
+		{
+			for (int y = 0; y < terrainData.heightmapHeight; y++)
+			{
+				heightMap[x,y] = heightMapImage.GetPixel((int)(x * heightMapScale.x),
+				                                (int)(y*heightMapScale.z)).grayscale * heightMapScale.y;
+			}
+		}
+		terrainData.SetHeights(0,0,heightMap);
+
+	}
+	public void resetTerrain(){
+
+        float[,] heightMap = new float[terrainData.heightmapWidth,terrainData.heightmapHeight];
+
+		for (int x = 0; x < terrainData.heightmapWidth; x++)
+		{
+			for (int y = 0; y < terrainData.heightmapHeight; y++)
+			{
+				heightMap[x,y] = 0;
+			}
+		}
+		terrainData.SetHeights(0,0,heightMap);
+	}
+
+	private void OnEnable() {
+		Debug.Log("Initialising Terrain Data");
+		terrain = this.GetComponent<Terrain>();
+		terrainData = Terrain.activeTerrain.terrainData; //becare 
+	}
+
+
+	private void Awake() {
+		SerializedObject tagManager = new SerializedObject(
+			AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0]);
+		SerializedProperty tagsProp = tagManager.FindProperty("tags");
+
+		Addtag(tagsProp,"Terrain");
+		Addtag(tagsProp,"Cloud");
+		Addtag(tagsProp,"Shore");
+
+		tagManager.ApplyModifiedProperties();
+
+		this.gameObject.tag ="Terrain";
+	}
+
+	void Addtag(SerializedProperty tagsProp, string newTag){
+		bool found = false;
+		//look if tag exists
+		for (int i = 0; i < tagsProp.arraySize; i++)
+		{
+			SerializedProperty t = tagsProp.GetArrayElementAtIndex(i);
+			if(t.stringValue.Equals(newTag)){
+				found = true;
+				break;
+			} 
+		}
+		//if not add a new one
+		if (!found)
+		{
+			tagsProp.InsertArrayElementAtIndex(0);
+			SerializedProperty newTagProp = tagsProp.GetArrayElementAtIndex(0);
+			newTagProp.stringValue = newTag;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Editor/CustomTerrainEditor.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEditor;
+using EditorGUITable;
+
+[CustomEditor(typeof(CustomTerrain))]
+[CanEditMultipleObjects]
+
+public class CustomTerrainEditor : Editor {
+
+	//Properties-------/
+	SerializedProperty randomHeightRange;//reference for the script and the GUI
+	SerializedProperty heightMapScale;
+	SerializedProperty heightMapImage;
+
+	//Foldouts--------/
+	bool showRandom = false; 
+	bool showLoadHeights = false;
+
+	private void OnEnable() {
+		randomHeightRange = serializedObject.FindProperty("randomHeightRange");
+		heightMapScale = serializedObject.FindProperty("heightMapScale");
+		heightMapImage = serializedObject.FindProperty("heightMapImage");
+	}
+    
+	//The GUI Loop, it gets update
+	public override void OnInspectorGUI(){
+		serializedObject.Update();//F
+
+		CustomTerrain terrain = (CustomTerrain) target;//Reference for our SCRIPT
+
+        //Foldout on GUI
+		showRandom = EditorGUILayout.Foldout(showRandom,"Random Heights");
+		if (showRandom)//A foldout need to be followed by a if statement
+		{
+			EditorGUILayout.LabelField("",GUI.skin.horizontalSlider);
+			GUILayout.Label("Set Heights Between Random Values", EditorStyles.boldLabel);
+			EditorGUILayout.PropertyField(randomHeightRange); //!! link GUI values to our variable
+			if (GUILayout.Button("Random Heights"))
+			{
+				terrain.RandomTerrain();//Method from our customTerrain Script to update our terrain.
+			}
+		}
+        
+		//Foldout to HeightMap
+		showLoadHeights = EditorGUILayout.Foldout(showLoadHeights,"Load Heights");
+		if (showLoadHeights)//A foldout need to be followed by a if statement
+		{
+			EditorGUILayout.LabelField("",GUI.skin.horizontalSlider);
+			GUILayout.Label("Load Heights From Texture", EditorStyles.boldLabel);
+			EditorGUILayout.PropertyField(heightMapImage);
+			EditorGUILayout.PropertyField(heightMapScale);
+			if (GUILayout.Button("Load Texture"))
+			{
+				terrain.LoadTexture();
+			}
+		}
+
+
+        
+		//resetButton
+		EditorGUILayout.LabelField("",GUI.skin.horizontalSlider);
+		if (GUILayout.Button("Reset Terrain"))
+		{
+			terrain.resetTerrain();
+		}
+		
+
+
+
+		serializedObject.ApplyModifiedProperties();//L
+	}
+}
